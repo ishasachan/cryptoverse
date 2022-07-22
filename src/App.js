2825f447186e9eb6a89b7ffaca10d101ff9c82da
ishasachan <ishasachan30@gmail.com>
2022-07-22 18:43:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component, Fragment} from 'react';
+import {Switch , Route , Link} from 'react-router-dom';
+import { Layout , Typography , Space} from 'antd';
+import {Navbar } from './components';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+  return  (
+  <div className='app'>
+
+<div className="navbar">
+<Navbar />
+
+</div>
+<div className='main'>
+
+</div>
+
+<div className='footer'>
+
+</div>
+    </div>
+    );
+}
+
+export default App
